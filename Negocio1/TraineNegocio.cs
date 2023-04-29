1a27b227714073277c3e3f3d8d135fe17b71a522
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2023-04-29 11:07:21 -0300

    seteando carpetas nuevamente

---
+++ Negocio1/TraineNegocio.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using Dominio1;
+
+namespace Negocio1
+{
+    public class TraineNegocio
+    {
+        public void Actualizar(Trainee user)
+        {
+
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+
+
+                datos.setearConsulta("Update Users set ImagenPerfil = @imagen ,Nombre = @nombre,Apellido =@apellido,fechaNacimiento =@fecha where Id = @id");
+                datos.setearParametro("@imagen", user.ImagenPerfil != null ? user.ImagenPerfil : (object)DBNull.Value);
+                datos.setearParametro("@nombre", user.Nombre);
+                datos.setearParametro("@fecha", user.fechaNacimiento);
+                datos.setearParametro("@apellido", user.Apellido);
+                datos.setearParametro("@id", user.Id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarLectura();
+            }
+
+        }
+
+        public int insertarNuevo(Trainee nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearProcedimiento("insertarNuevo");
+
+                datos.setearParametro("@email", nuevo.Email);
+                datos.setearParametro("@pass", nuevo.Pass);
+                return datos.ejecutarAccionEscala();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarLectura();
+            }
+
+
+
+        }
+
+        public bool Login(Trainee trainee)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("select id, email,pass, admin,imagenPerfil,nombre,apellido,fechaNacimiento from USERS where email  = @email And pass = @pass");
+
+                datos.setearParametro("@email", trainee.Email);
+                datos.setearParametro("@Pass", trainee.Pass);
+                datos.ejecutarLectura();
+                if (datos.Lector.Read())
+                {
+                    trainee.Id = (int)datos.Lector["id"];
+                    trainee.Admin = (bool)datos.Lector["admin"];
+                    if (!(datos.Lector["imagenPerfil"] is DBNull))
+                        trainee.ImagenPerfil = (string)datos.Lector["imagenPerfil"];
+                    if (!(datos.Lector["nombre"] is DBNull))
+                        trainee.Nombre = (string)datos.Lector["nombre"];
+                    if (!(datos.Lector["apellido"] is DBNull))
+                        trainee.Apellido = (string)datos.Lector["apellido"];
+                    if (!(datos.Lector["fechaNacimiento"] is DBNull))
+                        trainee.fechaNacimiento = DateTime.Parse(datos.Lector["fechaNacimiento"].ToString());
+
+
+
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarLectura();
+            }
+        }
+
+    }
+}
